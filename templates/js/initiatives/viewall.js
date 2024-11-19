0942ba04174270b64a2df3bcee09f26a1bb1b4e8
Savecoders <pablopincay2003@gmail.com>
2024-11-19 12:34:53 -0500

    fix: remove template component to createElement defautl

--- templates/js/initiatives/viewall.js
+++ templates/js/initiatives/viewall.js
@@ -61,71 +61,144 @@ const data = [
   },
 ];
 
-const dataCopy = Object.assign([], data);
-
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 // tradicional
 // const containerCard = document.querySelector('.container__cards');
+// or
+// const searchInput = document.getElementById('searchInput');
+// const btnSearch = document.getElementById('btnSearch');
 
 const containerCard = $('.container__cards');
-
-function createCard({ title, description, image, users, links, hearts, images }) {
-  const card = `
-            <aside class="card">
-            <div class="media__row">
-            <figure class="media__item">
-            <img src="../../assets/icons/users.svg" alt="user-icon" />
-            <small>${users}</small>
-            </figure>
-            <figure class="media__item">
-            <img src="../../assets/icons/link-2.svg" alt="link-icon" />
-            <small>${links}</small>
-            </figure>
-            <figure class="media__item">
-            <img src="../../assets/icons/heart-pulse.svg" alt="heart-icon" />
-            <small>${hearts}</small>
-              </figure>
-              <figure class="media__item">
-                <img src="../../assets/icons/image.svg" alt="image-icon" />
-                <small>${images}</small>
-              </figure>
-            </div>
-            <article class="card__content">
-              <a class="card__title" href="./view.html">${title}</a>
-              <p class="card__description">
-                ${description}
-                </p>
-                </article>
-                <picture class="card__picture">
-                <img src=${image} alt=${title} />
-                </picture>
-                </aside>`;
-
-  containerCard.innerHTML += card;
-}
-
+const searchInput = $('#searchInput');
+const btnSearch = $('#btnSearch');
 const btnLoadMore = document.getElementById('loadMore');
+
+const componentDefaultCard = ({ title, description, image, users, links, hearts, images }) => {
+  const asideNode = document.createElement('aside');
+  asideNode.classList.add('card');
+
+  const mediaRowNode = document.createElement('div');
+  mediaRowNode.classList.add('media__row');
+
+  const mediaItemUserNode = document.createElement('figure');
+  mediaItemUserNode.classList.add('media__item');
+
+  const userIconNode = document.createElement('img');
+  userIconNode.src = '../../assets/icons/users.svg';
+  userIconNode.alt = 'user-icon';
+
+  const userNode = document.createElement('small');
+  userNode.textContent = users;
+
+  mediaItemUserNode.appendChild(userIconNode);
+  mediaItemUserNode.appendChild(userNode);
+
+  const mediaItemLinkNode = document.createElement('figure');
+  mediaItemLinkNode.classList.add('media__item');
+
+  const linkIconNode = document.createElement('img');
+  linkIconNode.src = '../../assets/icons/link-2.svg';
+  linkIconNode.alt = 'link-icon';
+
+  const linkNode = document.createElement('small');
+  linkNode.textContent = links;
+
+  mediaItemLinkNode.appendChild(linkIconNode);
+  mediaItemLinkNode.appendChild(linkNode);
+
+  const mediaItemHeartNode = document.createElement('figure');
+  mediaItemHeartNode.classList.add('media__item');
+
+  const heartIconNode = document.createElement('img');
+  heartIconNode.src = '../../assets/icons/heart-pulse.svg';
+  heartIconNode.alt = 'heart-icon';
+
+  const heartNode = document.createElement('small');
+  heartNode.textContent = hearts;
+
+  mediaItemHeartNode.appendChild(heartIconNode);
+  mediaItemHeartNode.appendChild(heartNode);
+
+  const mediaItemImageNode = document.createElement('figure');
+  mediaItemImageNode.classList.add('media__item');
+
+  const imageIconNode = document.createElement('img');
+  imageIconNode.src = '../../assets/icons/image.svg';
+  imageIconNode.alt = 'image-icon';
+
+  const imageNode = document.createElement('small');
+  imageNode.textContent = images;
+
+  mediaItemImageNode.appendChild(imageIconNode);
+  mediaItemImageNode.appendChild(imageNode);
+
+  mediaRowNode.appendChild(mediaItemUserNode);
+  mediaRowNode.appendChild(mediaItemLinkNode);
+  mediaRowNode.appendChild(mediaItemHeartNode);
+  mediaRowNode.appendChild(mediaItemImageNode);
+
+  const cardArticuleNode = document.createElement('article');
+  cardArticuleNode.classList.add('card__content');
+
+  const cardTitleNode = document.createElement('a');
+  cardTitleNode.classList.add('card__title');
+  cardTitleNode.href = './view.html';
+  cardTitleNode.textContent = title;
+
+  const cardDescriptionNode = document.createElement('p');
+  cardDescriptionNode.classList.add('card__description');
+  cardDescriptionNode.textContent = description;
+
+  cardArticuleNode.appendChild(cardTitleNode);
+  cardArticuleNode.appendChild(cardDescriptionNode);
+
+  const cardPictureNode = document.createElement('picture');
+  cardPictureNode.classList.add('card__picture');
+
+  const cardPictureImgNode = document.createElement('img');
+  cardPictureImgNode.src = image;
+  cardPictureImgNode.alt = title;
+
+  cardPictureNode.appendChild(cardPictureImgNode);
+  cardArticuleNode.appendChild(cardTitleNode);
+  cardArticuleNode.appendChild(cardDescriptionNode);
+
+  asideNode.appendChild(mediaRowNode);
+  asideNode.appendChild(cardArticuleNode);
+  asideNode.appendChild(cardPictureNode);
+
+  asideNode.addEventListener('click', () => {
+    if (asideNode.classList.contains('card--clicked')) {
+      asideNode.classList.remove('card--clicked');
+    } else {
+      asideNode.classList.add('card--clicked');
+    }
+  });
+
+  return asideNode;
+};
+
+const addCard = card => {
+  containerCard.appendChild(card);
+};
+
+const getRandomCardValue = () => {
+  return Math.floor(Math.random() * data.length);
+};
 
 // patron observer
 btnLoadMore.addEventListener('click', () => {
   const childrens = containerCard.children.length;
-
   if (childrens < 25) {
     for (let i = 0; i < 6; i++) {
-      createCard(data[1]);
+      addCard(componentDefaultCard(data[getRandomCardValue()]));
     }
   } else {
     alert('no more iniciativas');
   }
 });
-
-// search input and button
-
-const searchInput = $('#searchInput');
-
-const btnSearch = $('#btnSearch');
 
 const filterCardInContainer = textCriteria => {
   // convert NodeList to array -> containerCard.children is a NodeList
@@ -135,16 +208,19 @@ const filterCardInContainer = textCriteria => {
     return title.includes(textCriteria);
   });
 
-  containerCard.innerHTML = '';
+  // containerCard.innerHTML = '';
+  while (containerCard.lastElementChild) {
+    containerCard.removeChild(containerCard.lastElementChild);
+  }
+
   getFindsElements.forEach(el => {
-    // append child | agregar un elemento hijo
     containerCard.appendChild(el);
   });
 };
 
 const pageLoad = () => {
-  data.forEach(el => {
-    createCard(el);
+  data.forEach(card => {
+    addCard(componentDefaultCard(card));
   });
 };
 
@@ -157,17 +233,6 @@ btnSearch.addEventListener('click', () => {
     pageLoad();
   }
 });
+
 //siempre se va a ejecutar la pagina
 pageLoad();
-
-const cards = $$('.card');
-
-cards.forEach(card => {
-  card.addEventListener('click', () => {
-    if (card.classList.contains('card--clicked')) {
-      card.classList.remove('card--clicked');
-    } else {
-      card.classList.add('card--clicked');
-    }
-  });
-});
