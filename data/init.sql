05e7a93862cd9e7f0ba3d75b60c13663cf4081d7
Savecoders <pablopincay2003@gmail.com>
2024-12-27 20:56:27 -0500

    chore: database init sql

---
+++ data/init.sql
@@ -0,0 +1,410 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3308
+-- Tiempo de generación: 04-12-2024 a las 02:52:08
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_daw`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actividades`
+--
+
+CREATE TABLE `actividades` (
+  `ID` bigint(20) NOT NULL,
+  `iniciativa_id` bigint(20) DEFAULT NULL,
+  `nombre` text NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `ID` bigint(20) NOT NULL,
+  `post_id` bigint(20) DEFAULT NULL,
+  `autor_id` bigint(20) DEFAULT NULL,
+  `contenido` text NOT NULL,
+  `fecha_comentario` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entidad_tags`
+--
+
+CREATE TABLE `entidad_tags` (
+  `entidad_id` bigint(20) NOT NULL,
+  `tag_id` bigint(20) NOT NULL,
+  `tipo` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fotos`
+--
+
+CREATE TABLE `fotos` (
+  `ID` bigint(20) NOT NULL,
+  `url` text NOT NULL,
+  `post_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `iniciativas`
+--
+
+CREATE TABLE `iniciativas` (
+  `ID` bigint(20) NOT NULL,
+  `nombre` text NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `logo` text DEFAULT NULL,
+  `creador_id` bigint(20) DEFAULT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `iniciativa_tags`
+--
+
+CREATE TABLE `iniciativa_tags` (
+  `iniciativa_id` bigint(20) NOT NULL,
+  `tag_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `me_encanta`
+--
+
+CREATE TABLE `me_encanta` (
+  `usuario_id` bigint(20) NOT NULL,
+  `post_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `posts`
+--
+
+CREATE TABLE `posts` (
+  `ID` bigint(20) NOT NULL,
+  `iniciativa_id` bigint(20) DEFAULT NULL,
+  `autor_id` bigint(20) DEFAULT NULL,
+  `contenido` text NOT NULL,
+  `fecha_publicacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `red_social`
+--
+
+CREATE TABLE `red_social` (
+  `ID` bigint(20) NOT NULL,
+  `nombre` text NOT NULL,
+  `url` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seguimientos`
+--
+
+CREATE TABLE `seguimientos` (
+  `usuario_id` bigint(20) NOT NULL,
+  `iniciativa_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tags`
+--
+
+CREATE TABLE `tags` (
+  `ID` bigint(20) NOT NULL,
+  `nombre` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID` bigint(20) NOT NULL,
+  `nombre` text NOT NULL,
+  `email` text NOT NULL,
+  `clave` text NOT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_redes`
+--
+
+CREATE TABLE `usuario_redes` (
+  `usuario_id` bigint(20) NOT NULL,
+  `red_social_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `actividades`
+--
+ALTER TABLE `actividades`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `iniciativa_id` (`iniciativa_id`);
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `autor_id` (`autor_id`);
+
+--
+-- Indices de la tabla `entidad_tags`
+--
+ALTER TABLE `entidad_tags`
+  ADD PRIMARY KEY (`entidad_id`,`tag_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indices de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `post_id` (`post_id`);
+
+--
+-- Indices de la tabla `iniciativas`
+--
+ALTER TABLE `iniciativas`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `creador_id` (`creador_id`);
+
+--
+-- Indices de la tabla `iniciativa_tags`
+--
+ALTER TABLE `iniciativa_tags`
+  ADD PRIMARY KEY (`iniciativa_id`,`tag_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indices de la tabla `me_encanta`
+--
+ALTER TABLE `me_encanta`
+  ADD PRIMARY KEY (`usuario_id`,`post_id`),
+  ADD KEY `post_id` (`post_id`);
+
+--
+-- Indices de la tabla `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `iniciativa_id` (`iniciativa_id`),
+  ADD KEY `autor_id` (`autor_id`);
+
+--
+-- Indices de la tabla `red_social`
+--
+ALTER TABLE `red_social`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `seguimientos`
+--
+ALTER TABLE `seguimientos`
+  ADD PRIMARY KEY (`usuario_id`,`iniciativa_id`),
+  ADD KEY `iniciativa_id` (`iniciativa_id`);
+
+--
+-- Indices de la tabla `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `nombre` (`nombre`) USING HASH;
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `email` (`email`) USING HASH;
+
+--
+-- Indices de la tabla `usuario_redes`
+--
+ALTER TABLE `usuario_redes`
+  ADD PRIMARY KEY (`usuario_id`,`red_social_id`),
+  ADD KEY `red_social_id` (`red_social_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `actividades`
+--
+ALTER TABLE `actividades`
+  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `iniciativas`
+--
+ALTER TABLE `iniciativas`
+  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `red_social`
+--
+ALTER TABLE `red_social`
+  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `actividades`
+--
+ALTER TABLE `actividades`
+  ADD CONSTRAINT `actividades_ibfk_1` FOREIGN KEY (`iniciativa_id`) REFERENCES `iniciativas` (`ID`);
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`ID`),
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`autor_id`) REFERENCES `usuarios` (`ID`);
+
+--
+-- Filtros para la tabla `entidad_tags`
+--
+ALTER TABLE `entidad_tags`
+  ADD CONSTRAINT `entidad_tags_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`ID`),
+  ADD CONSTRAINT `entidad_tags_iniciativa_id_fkey` FOREIGN KEY (`entidad_id`) REFERENCES `iniciativas` (`ID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `entidad_tags_post_id_fkey` FOREIGN KEY (`entidad_id`) REFERENCES `posts` (`ID`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  ADD CONSTRAINT `fotos_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`ID`);
+
+--
+-- Filtros para la tabla `iniciativas`
+--
+ALTER TABLE `iniciativas`
+  ADD CONSTRAINT `iniciativas_ibfk_1` FOREIGN KEY (`creador_id`) REFERENCES `usuarios` (`ID`);
+
+--
+-- Filtros para la tabla `iniciativa_tags`
+--
+ALTER TABLE `iniciativa_tags`
+  ADD CONSTRAINT `iniciativa_tags_ibfk_1` FOREIGN KEY (`iniciativa_id`) REFERENCES `iniciativas` (`ID`),
+  ADD CONSTRAINT `iniciativa_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`ID`);
+
+--
+-- Filtros para la tabla `me_encanta`
+--
+ALTER TABLE `me_encanta`
+  ADD CONSTRAINT `me_encanta_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`ID`),
+  ADD CONSTRAINT `me_encanta_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`ID`);
+
+--
+-- Filtros para la tabla `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`iniciativa_id`) REFERENCES `iniciativas` (`ID`),
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`autor_id`) REFERENCES `usuarios` (`ID`),
+  ADD CONSTRAINT `posts_ibfk_3` FOREIGN KEY (`autor_id`) REFERENCES `usuarios` (`ID`);
+
+--
+-- Filtros para la tabla `seguimientos`
+--
+ALTER TABLE `seguimientos`
+  ADD CONSTRAINT `seguimientos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`ID`),
+  ADD CONSTRAINT `seguimientos_ibfk_2` FOREIGN KEY (`iniciativa_id`) REFERENCES `iniciativas` (`ID`);
+
+--
+-- Filtros para la tabla `usuario_redes`
+--
+ALTER TABLE `usuario_redes`
+  ADD CONSTRAINT `usuario_redes_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`ID`),
+  ADD CONSTRAINT `usuario_redes_ibfk_2` FOREIGN KEY (`red_social_id`) REFERENCES `red_social` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
