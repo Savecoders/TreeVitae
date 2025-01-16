06c9902e87eb220f65063560a150a637bfb8c3d5
Savecoders <pablopincay2003@gmail.com>
2025-01-16 00:36:07 -0500

    chore: default controllers class

--- controllers/ComentarioController.php
+++ controllers/ComentarioController.php
@@ -1,1 +1,16 @@
 <?php
+class IndexController
+{
+    public function index()
+    {
+        if (!empty($_GET['p'])) {
+            $page =  $_GET['p']; // limpiar datos
+            // flujo de ventanas
+            require_once 'views/static/' . $page . '.php';
+        } else {
+            // flujo de ventanas
+            require_once 'views/home.php'; //mostrando la vista de home de la aplicacion
+
+        }
+    }
+}
--- controllers/IniciativaController.php
+++ controllers/IniciativaController.php
@@ -1,1 +1,16 @@
 <?php
+class IniciativaController
+{
+    private $model;
+
+    public function __construct()
+    {
+        // $this->model = new IniciativaRepository();
+    }
+
+    public function viewall()
+    {
+        // $iniciativas = $this->model->getAll();
+        require_once 'views/iniciativa/iniciativa.viewall.php';
+    }
+}
