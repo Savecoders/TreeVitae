9503f0be7751351d3e6c4e65571b83cc0dd74cd0
Savecoders <pablopincay2003@gmail.com>
2025-01-16 00:36:32 -0500

    lint: fix lint php storage

--- core/Config.php
+++ core/Config.php
@@ -1,21 +1,20 @@
 <?php
 //controlador y funcion predefinida
-define("CONTROLADOR_PRINCIPAL","index");
+define("CONTROLADOR_PRINCIPAL", "index");
 define("FUNCION_PRINCIPAL", "index");
 
 //ruta de templates
-define("HEADER", 'view/templates/header.php');
-define("FOOTER", 'view/templates/footer.php');
+define("HEADER", 'views/templates/navbar.php');
+define("FOOTER", 'views/templates/footer.php');
 
 
 // ruta de vistas modulo productos
-define("VPRODUCTOS", "view/productos/productos.");
+define("VPRODUCTOS", "views/productos/productos.");
 
 // ruta de vistas modulo categorias
-define("VCATEGORIAS", "view/categorias/categorias.");
+define("VCATEGORIAS", "views/categorias/categorias.");
 
 // conexion bb
-define("DBNAME","mvc");
-define("DBUSER","root");
-define("DBPASSWORD","123456");
-
+define("DBNAME", "mvc");
+define("DBUSER", "root");
+define("DBPASSWORD", "123456");
