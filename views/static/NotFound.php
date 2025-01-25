5a7e4e31061586085796a6e1927125fe9505a3a6
Savecoders <pablopincay2003@gmail.com>
2025-01-25 11:32:02 -0500

    feat: not found page

---
+++ views/static/NotFound.php
@@ -0,0 +1,29 @@
+<?php require_once HEADER; ?>
+
+<style>
+    .container__404 {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 50px;
+    }
+
+    .title__principal {
+        text-align: center;
+        margin-top: 20px;
+        color: var(--text-base);
+    }
+</style>
+
+<main class="main__container__content">
+
+    <div class="container__404">
+        <image src="public/assets/images/404.svg" alt="Error 404" />
+    </div>
+    <h1 class="title__principal">Hey!!! Algo paso :(</h1>
+
+    <!-- volver a la pagina anterior -->
+    <a href="javascript:history.back()" class="btn primary__with-icon">Regresa a la pagina Anterior!</a>
+</main>
+
+<?php require_once FOOTER; ?>
